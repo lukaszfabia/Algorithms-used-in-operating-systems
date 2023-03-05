df6ac856ad0938dd7acc1f54cb43192d0c2c5674
LukaszAcer <ufabia03@gmail.com>
2023-03-05 21:49:31 +0100

    commit

--- src/Process.java
+++ src/Process.java
@@ -1,12 +1,26 @@
 public class Process {
     private int executeTime;
     private int arrivalTime;
+    private int remainingTime;
+    private int waitingTime;
+    private int startTime;
     private final int nr;
 
     public Process(int executeTime, int arrivalTime, int nr) {
         this.executeTime = executeTime;
         this.arrivalTime = arrivalTime;
         this.nr = nr;
+        this.waitingTime=0;
+        this.startTime=0;
+        this.remainingTime =executeTime;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
     }
 
     public int getExecuteTime() {
@@ -21,6 +35,22 @@ public class Process {
         this.executeTime = executeTime;
     }
 
+    public void setRemainingTime(int waitingTime) {
+        this.remainingTime = waitingTime;
+    }
+
+    public int getRemainingTime() {
+        return remainingTime;
+    }
+
+    public int getWaitingTime() {
+        return waitingTime;
+    }
+
+    public void setWaitingTime(int waitingTime) {
+        this.waitingTime = waitingTime;
+    }
+
     public void setArrivalTime(int arrivalTime) {
         this.arrivalTime = arrivalTime;
     }
@@ -28,9 +58,20 @@ public class Process {
     public int getNr() {
         return nr;
     }
+    public boolean isCompleted() {
+        return remainingTime == 0;
+    }
+
+    public void execute() {
+        remainingTime--;
+    }
 
     @Override
     public String toString() {
         return String.format("%-20d %-20d %-20d",getNr(), getArrivalTime(),getExecuteTime());
+    }
+
+    public void incrementWaitingTime() {
+        waitingTime++;
     }
 }
--- src/ProcessSimulator.java
+++ src/ProcessSimulator.java
@@ -1,38 +1,43 @@
 import java.util.*;
 
 public class ProcessSimulator {
-    private final List<Process> processes;
+    private List<Process> processList1;
+    private List<Process> processList;
 
     public ProcessSimulator(int amount) {
-        this.processes = new ArrayList<>();
+        this.processList1 = new ArrayList<>();
         Random r = new Random();
 
         for (int i = 0; i < amount; i++) {
-            processes.add(new Process(r.nextInt(100), r.nextInt(200), (i + 1)));
+            processList1.add(new Process(r.nextInt(100), r.nextInt(200), (i + 1)));
         }
     }
 
+    public ProcessSimulator(List<Process> processList) {
+        this.processList = processList;
+    }
+
     public List<Process> getProcesses() {
-        return processes;
+        return processList;
     }
 
     public void showData() {
-        for (Process process : processes) {
+        for (Process process : processList) {
             System.out.println(process);
         }
         System.out.println();
     }
 
     public void fcfsSimulation() {
-        processes.sort(Comparator.comparing(Process::getArrivalTime).thenComparing(Process::getNr));
+        processList.sort(Comparator.comparing(Process::getArrivalTime).thenComparing(Process::getNr));
         int currentTime = 0;
         double totalWaitingTime = 0;
 
-        for (int i = 0; i < processes.size() - 1; i++) {
-            currentTime += processes.get(i).getExecuteTime();
+        for (int i = 0; i < processList.size() - 1; i++) {
+            currentTime += processList.get(i).getExecuteTime();
             try {
                 Thread.sleep(currentTime);
-                System.out.println("Process " + (i + 1) + ": " + currentTime+" ms");
+                System.out.println("Process " + (i + 1) + ": " + currentTime + " ms");
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
@@ -43,27 +48,96 @@ public class ProcessSimulator {
 
 
     public void sjfSimulation() {
-        int currentTime = 0;
         double totalWaitingTime = 0;
 
-        processes.sort(Comparator.comparingInt(Process::getExecuteTime).thenComparing(Process::getNr));
+        processList.sort(Comparator.comparing(Process::getArrivalTime).thenComparing(Process::getExecuteTime).thenComparing(Process::getNr));
 
-        for (int i = 0; i < processes.size() - 1; i++) {
-            currentTime += processes.get(i).getExecuteTime();
-            try {
-                Thread.sleep(currentTime);
-                System.out.println("Process " + (i + 1) + ": " + currentTime+" ms");
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            totalWaitingTime += currentTime;
+        for (Process process : processList) {
+            totalWaitingTime += process.getExecuteTime();
         }
 
-        System.out.printf("%s %.2f %s\n", "Average waiting time for SJF: ", totalWaitingTime / getProcesses().size(),"ms");
+        System.out.printf("%s %.2f %s\n", "Total waiting time: ", (totalWaitingTime - processList.get(0).getArrivalTime()), " ms");
+        System.out.printf("%s %.2f %s\n", "Average waiting time for SJF: ", (totalWaitingTime - processList.get(0).getArrivalTime()) / getProcesses().size(), "ms");
     }
 
-    public void rrSimulation(int quantumTime) {
+    public void rrSimulation(int quantum) {
+        Queue<Process> queue = new LinkedList<>(processList);
+        int time = 0;
+        double ave = 0;
+        while (!queue.isEmpty()) {
+            Process process = queue.poll();
+            int remainingTime = process.getExecuteTime() - quantum;
+            time += quantum;
+            if (remainingTime > 0) {
+                process.setExecuteTime(remainingTime);
+                queue.add(process);
+            } else {
+                time += remainingTime;
+                System.out.println(process.getNr() + " zakończony w czasie " + time);
+                ave += time;
+            }
+        }
+        System.out.printf("%s %.2f %s\n", "Total waiting time for RR: ", ave, " ms");
+        System.out.printf("%s %.2f %s\n", "Average waiting time for RR: ", ave / processList.size(), " ms");
+    }
+
+    public void srtfSimulation() {
+        int n = processList.size();
+
+        int[] remainingTime = new int[n];
+        int[] waitingTime = new int[n];
+
+        // kopiowanie czasów trwania procesów do remainingTime
+        for (int i = 0; i < n; i++) {
+            remainingTime[i] = processList.get(i).getExecuteTime();
+        }
+
+        int complete = 0; // liczba zakończonych procesów
+        int t = 0; // aktualny czas
+        int minRemainingTimeIndex; // indeks procesu o najkrótszym pozostałym czasie
+
+        while (complete != n) {
+            minRemainingTimeIndex = -1;
+
+            // szukanie procesu o najkrótszym pozostałym czasie
+            for (int i = 0; i < n; i++) {
+                if (remainingTime[i] > 0 && (minRemainingTimeIndex == -1 || remainingTime[i] < remainingTime[minRemainingTimeIndex])) {
+                    minRemainingTimeIndex = i;
+                }
+            }
+
+            if (minRemainingTimeIndex == -1) {
+                t++;
+                continue;
+            }
+
+            remainingTime[minRemainingTimeIndex]--;
+
+            if (remainingTime[minRemainingTimeIndex] == 0) {
+                complete++;
+
+                int completionTime = t + 1;
+                int turnaroundTime = completionTime - processList.get(minRemainingTimeIndex).getArrivalTime();
+                int waitingTimeProcess = turnaroundTime - processList.get(minRemainingTimeIndex).getExecuteTime();
+
+                waitingTime[minRemainingTimeIndex] = Math.max(waitingTimeProcess, 0);
+            }
+
+            t++;
+        }
+
+        // obliczanie średniego czasu oczekiwania procesu w kolejce
+        int totalWaitingTime = 0;
+
+        for (int i = 0; i < n; i++) {
+            totalWaitingTime += waitingTime[i];
+        }
+
+        double averageWaitingTime = (double) totalWaitingTime / n;
+        System.out.println("Średni czas oczekiwania procesu w kolejce: " + averageWaitingTime);
+
 
     }
+
 
 }
