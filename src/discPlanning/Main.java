fbb626fdc82d413176b97b39898ede854e15017d
LukaszAcer <ufabia03@gmail.com>
2023-03-15 14:31:22 +0100

    skonczenie wszytkich algo

--- src/discPlanning/Main.java
+++ src/discPlanning/Main.java
@@ -3,7 +3,7 @@ package discPlanning;
 
 public class Main {
     public static void main(String[] args) {
-        Results r = new Results(1, 10, 10);
+        Results r = new Results(2, 500, 20);
         r.showData();
 
     }
