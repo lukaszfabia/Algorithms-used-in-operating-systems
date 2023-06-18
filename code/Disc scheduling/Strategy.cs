f15ba8b756cb1c5bd00102bc0a0d59f6bcc559ef
Lukasz Fabia <114498264+lukaszfabia@users.noreply.github.com>
2023-06-18 11:15:19 +0200

    Delete Strategy.cs

--- code/Disc scheduling/Strategy.cs
+++
@@ -1,8 +0,0 @@
-interface IStrategy{
-    void Fcfs(List<Task> tasks);
-    void Sstf(List<Task> tasks);
-    void Scan(List<Task> tasks);
-    void CScan(List<Task> tasks);
-    void Edf(List<Task> tasks);
-    void FdScan(List<Task> tasks);
-}
